b8104a532fc204cfe8a2f35168870cd721acffc9
Enzo Liu <liuenze6516@gmail.com>
2017-01-25 09:51:39 +0800

    init algorithm4

---
+++ algorithm4/percolation/Percolation.java
@@ -0,0 +1,51 @@
+/******************************************************************************
+ *  Name:    Kevin Wayne
+ *  NetID:   wayne
+ *  Precept: P01
+ *
+ *  Partner Name:    N/A
+ *  Partner NetID:   N/A
+ *  Partner Precept: N/A
+ *
+ *  Description:  Modeling Percolation using an N-by-N grid and Union-Find data
+ *                structures to determine the threshold. woot. woot.
+ ******************************************************************************/
+
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+public class Percolation {
+    // create n-by-n grid, with all sites blocked
+    public Percolation(int n) {
+
+    }
+    // open site (row, col) if it is not open already
+    public    void open(int row, int col) {
+
+    }
+
+    // is site (row, col) open?
+    public boolean isOpen(int row, int col) {
+
+    }
+
+    // is site (row, col) full?
+    public boolean isFull(int row, int col) {
+
+    }
+
+    // number of open sites
+    public     int numberOfOpenSites() {
+
+    }
+
+    // does the system percolate?
+    public boolean percolates() {
+    }
+
+    // test client (optional)
+    public static void main(String[] args) {
+
+    }
+}
